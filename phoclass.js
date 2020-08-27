c935d97f88d32a904fc88f18bfc63642b5521aaa
Paul <ipauleh@gmail.com>
2020-08-27 14:55:47 +0100

    fixed a bug, set up tensors

--- phoclass.js
+++ phoclass.js
@@ -1,13 +1,25 @@
 let mic, fft, train, label;
+
+let training_x = [],
+	training_y = [],
+	testing_x = [],
+	testing_y = [];
+
 const classes = [ 'aah', 'eee', 'i' ];
 
 const inputSize = 1024; //defaults to 1024, needs to be power of 2.
 let sinputShape = Math.sqrt(inputSize);
 let numClasses = classes.length;
 
+// Training Paras
 const epochs = 40;
 const learningRate = 0.2;
 const testRatio = 0.2;
+const batchSize = 10;
+
+// Classification Paras
+const predictionConfidence = 0.7;
+const predictionInterval = 0;
 
 function setup() {
 	train = [];
@@ -19,6 +31,13 @@ function setup() {
 	mic.start();
 	fft = new p5.FFT();
 	fft.setInput(mic);
+}
+
+function touchStarted() {
+	if (getAudioContext().state !== 'running') {
+		getAudioContext().resume();
+		console.log('Audio Resumed!');
+	}
 }
 
 function draw() {
@@ -48,10 +67,10 @@ function train_nn() {
 	// create a conv neural network
 	const m = tf.sequential({
 		layers: [
-			tf.layers.conv2d({
-				inputShape: [ sinputShape, sinputShape, 1 ],
+			tf.layers.conv1d({
+				inputShape: [ 1, sinputShape, sinputShape ],
 				activation: 'relu',
-				kernelSize: [ 3, 3 ],
+				kernelSize: 3,
 				filters: 64
 			}),
 			tf.layers.conv2d({
@@ -116,26 +135,44 @@ function train_nn() {
 		metrics: [ 'accuracy' ]
 	});
 
+	m.summary();
+
+	// x = data, y = label
+
 	for (let x = 0; x < train.length; x++) {
+		// randomly split the test/training data
 		if (Math.random() < testRatio) {
 			// valdiation data
+			testing_x.push(train[x]);
+			testing_y.push(label[x]);
 		} else {
 			// train data
+			training_x.push(train[x]);
+			training_y.push(label[x]);
 		}
 	}
 
-	// load data into tensors
+	console.log('Fitting Data.. ');
 
-	//for (let x = 0; x < train.length; x++) {}
+	const train_t_x = tf.tensor(training_x, [ training_x.length, 32, 32 ]);
+	const train_t_y = tf.tensor(training_y);
 
-	// split into training and testing data.
+	const test_t_x = tf.tensor(testing_x, [ testing_x.length, 32, 32 ]);
+	const test_t_y = tf.tensor(testing_y);
 
 	//fit
+
+	fit(train_t_x, train_t_y, test_t_x, test_t_y, m).then(() => {
+		console.log('Complete');
+	});
 
 	//save
 }
 
-train_nn();
+async function fit(train_x, train_y, test_x, test_y, model) {
+	const response = await model.fit(train_x, train_y);
+	console.log(response);
+}
 
 function classify() {
 	// every x frames classify the current phonetic input against the CNN.
